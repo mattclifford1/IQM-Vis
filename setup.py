dd923cc8d1a2fa5b47fb27a8700f15948a613bd4
matt clifford <matt.clifford@bristol.ac.uk>
2022-11-11 15:40:48 +0000

    full deps for metrics

--- setup.py
+++ setup.py
@@ -1,12 +1,18 @@
 # Author: Matt Clifford <matt.clifford@bristol.ac.uk>
 
 from setuptools import setup, find_packages
-import IQM_VIS
+# import IQM_VIS
 
 setup(name='IQM-VIS',
-      version='0.2.5',
+      version='0.2.5.1',
       packages=find_packages(),
-      install_requires=['numpy', 'opencv-python', 'scikit-image', 'PyQt6', 'matplotlib'],
+      install_requires=['numpy',
+                        'opencv-python',
+                        'scikit-image',
+                        'PyQt6',
+                        'matplotlib',
+                        'torch',
+                        'torchmetrics'],
       author="Matt Clifford",
       author_email="matt.clifford@bristol.ac.uk",
       description="Extendable user interface for the assessment of transformations on image metrics.")
