6459620ea7142cb47dfee9d50dc806d897ea87ac
matt clifford <matt.clifford@bristol.ac.uk>
2023-03-24 15:28:23 +0000

    bug fxes in package images and deps

--- setup.py
+++ setup.py
@@ -14,17 +14,22 @@ def get_version():
         ver = ver[:-1]
     return ver
 
+def dependencies_from_file(file_path):
+    required = []
+    with open(file_path) as f:
+        for l in f.readlines():
+            l_c = l.strip()
+            # get not empty lines and ones that do not start with python
+            # comment "#" (preceded by any number of white spaces)
+            if l_c and not l_c.startswith('#'):
+                required.append(l_c)
+    return required
+
 setup(name='IQM-Vis',
       version=get_version(),
       packages=find_packages(),
       include_package_data=True,
-      install_requires=['numpy',
-                        'opencv-python',
-                        'scikit-image',
-                        'PyQt6',
-                        'matplotlib',
-                        'torch',
-                        'torchmetrics'],
+      install_requires=dependencies_from_file('requirements.txt'),
       author="Matt Clifford",
       author_email="matt.clifford@bristol.ac.uk",
       description="Extendable user interface for the assessment of transformations on image metrics.",
