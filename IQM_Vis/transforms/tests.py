6780de0a41b04677f6a1b1158935580d5f8dd25c
Matt Clifford <matt.clifford@bristol.ac.uk>
2024-10-08 16:36:53 +0100

    more test cases for transforms

--- IQM_Vis/transforms/tests.py
+++ IQM_Vis/transforms/tests.py
@@ -4,20 +4,76 @@
 '''
 Test that all of the transforms work as expected
 '''
+import os
 import numpy as np
 from IQM_Vis.transforms import get_all_transforms
+from IQM_Vis.utils import load_image
 
-def test_all_transforms():
+
+def _get_test_image():
+    file_path = os.path.dirname(os.path.abspath(__file__))
+    im_path = os.path.join(file_path, '..', 'examples', 'images', 'waves2.jpeg')
+    return load_image(im_path)
+
+
+def test_all_transforms_init_value_no_change_image():
     # make a mock image
-    image = np.random.rand(256, 256, 3)
+    image = _get_test_image()
     # test that the default value doesn't change the image
     tol = 0.0
     all_transfoms = get_all_transforms()
     for name, transform in all_transfoms.items():
         if 'init_value' in transform:
-            assert np.allclose(transform['function'](image, transform['init_value']), image, rtol=tol)
-        # also test default in the arg defn
-        assert np.allclose(transform['function'](image), image, rtol=tol)
+            value = transform['init_value']
+        else:
+            value = 0
+        assert np.allclose(transform['function'](image, value), image, rtol=tol)
+
+
+def test_all_transforms_min_max_specified():
+    # make a mock image
+    image = _get_test_image()
+    # test that the default value doesn't change the image
+    tol = 0.0
+    all_transfoms = get_all_transforms()
+    # test that the min and max values work
+    for name, transform in all_transfoms.items():
+        assert 'min' in transform or 'max' in transform
+
+
+def test_all_transforms_min_change_image():
+    # make a mock image
+    image = _get_test_image()
+    # test that the default value doesn't change the image
+    tol = 0.0
+    all_transfoms = get_all_transforms()
+    # test that the min and max values work
+    for name, transform in all_transfoms.items():
+        if 'init_value' in transform:
+            if transform['init_value'] == transform['min']:
+                continue
+        else:
+            if transform['min'] == 0:
+                continue
+        assert not np.allclose(transform['function'](image, transform['min']), image, rtol=tol)
+
+
+def test_all_transforms_max_change_image():
+    # make a mock image
+    image = _get_test_image()
+    # test that the default value doesn't change the image
+    tol = 0.0
+    all_transfoms = get_all_transforms()
+    # test that the min and max values work
+    for name, transform in all_transfoms.items():
+        if 'init_value' in transform:
+            if transform['init_value'] == transform['max']:
+                continue
+        else:
+            if transform['max'] == 0:
+                continue
+        assert not np.allclose(transform['function'](image, transform['max']), image, rtol=tol)
+
 
 if __name__ == '__main__':
-    test_all_transforms()
+    test_all_transforms_max_change_image()
