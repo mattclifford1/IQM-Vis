cf6a979896783f5471e7850afe6eb4b331c0c902
Matt Clifford <mc15445@bristol.ac.uk>
2025-10-17 14:18:22 +0100

    version patch bump for python 3.13 support

--- IQM_Vis/version.py
+++ IQM_Vis/version.py
@@ -2,4 +2,4 @@
 # License: BSD 3-Clause License
 
 # Changing the version number will action GitHub to push to PyPi the new version
-__version__ = '1.0'
+__version__ = '1.0.1'  # format: MAJOR.MINOR.PATCH
