6e4117fe730c5cc134a864564a622f04f1137278
Matt Clifford <matt.clifford@bristol.ac.uk>
2025-11-27 12:09:26 +0000

    token info for twine

--- dev_resources/scripts/push_to_pypi.sh
+++ dev_resources/scripts/push_to_pypi.sh
@@ -5,4 +5,18 @@
 
 git clean -xfd
 python setup.py sdist bdist_wheel
+# make sure to add the IQM_Vis repository to your .pypirc file and create tokens on PyPI
+# e.g.
+# [distutils]
+#   index-servers =
+#     pypi
+#     IQM_Vis
+
+# [pypi]
+#   username = __token__
+#   password = pypi-token-goes-here
+# [IQM_Vis]
+#   repository = https://pypi.org/project/IQM-Vis/
+#   username = __token__
+#   password = pypi-token-goes-here
 twine upload dist/* --repository IQM_Vis
