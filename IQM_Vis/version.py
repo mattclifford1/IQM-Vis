78238f8b297dea1e1020cd750c22175109d3836b
Matt Clifford <matt.clifford@bristol.ac.uk>
2025-11-27 11:51:03 +0000

    bump version for new readme

--- IQM_Vis/version.py
+++ IQM_Vis/version.py
@@ -2,4 +2,4 @@
 # License: BSD 3-Clause License
 
 # Changing the version number will action GitHub to push to PyPi the new version
-__version__ = '1.0.1'  # format: MAJOR.MINOR.PATCH
+__version__ = '1.0.2'  # format: MAJOR.MINOR.PATCH
